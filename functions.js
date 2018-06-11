8277b634359904939bdd1629e1881b576673f7eb
unknown <texas_telecaster@hotmail.com>
2018-06-11 10:10:58 -0500

    test

--- functions.js
+++ functions.js
@@ -1,14 +1,53 @@
 'use strict'
+let DateList = [];
 
-const myFunctions = {}
+const moment = require('moment')
+const Month = require('calendar-months').Month;
+const weeks = Month.create(moment(new Date()).format('YYYY-MM')).calendarWeeks();
+const valid_day = require('@date/today')
 
-myFunctions.isNumber = number => {
-    return typeof(number)
+function getAllDates() {
+  DateList = [];
+  weeks.map(wk => {
+    wk.map(data => {
+      let yest = false;
+      let tomo = false;
+      if (moment(data).toDate().getDate() - moment(moment.now()).toDate().getDate() == -1) {
+        yest = true;
+      }
+      if (moment(data).toDate().getDate() - moment(moment.now()).toDate().getDate() == 1) {
+        tomo = true;
+      }
+      DateList.push({
+        xDay: moment(data).format('dddd').toUpperCase(),
+        nDay: moment(data).format('ddd'),
+        Day: moment(data).format('DD'),
+        nMonth: moment(data).format('MMM'),
+        Month: moment(data).format('MM'),
+        Year: moment(data).format('YYYY'),
+        moment: moment(data).unix(),
+        today: valid_day.isToday(moment(data).toDate()),
+        before: valid_day.isBeforeToday(moment(data).toDate()),
+        after: valid_day.isAfterToday(moment(data).toDate()),
+        tomo: tomo,
+        yest: yest
+      })
+    })
+  })
+
+  return DateList;
 }
 
-myFunctions.isVowel = item => {
-    return ['a', 'e', 'i', 'o', 'u']
-        .filter(vocal => vocal == item).length <= 0 ? false : true
+function getDate(moment) {
+  let theid = moment;
+
+  const result = DateList.filter(data => data.moment == theid);
+  return result;
+
 }
 
-module.exports = myFunctions
+module.exports = {
+  getAllDates,
+  getDate,
+  DateList
+}
--- test/mocha.test.js
+++ test/mocha.test.js
@@ -1,16 +1,22 @@
 'use strict'
 
 const chai = require('chai')
-const _function = require('../functions')
+const functions = require('../functions');
+
+const getAllDates = functions.getAllDates();
+let date_moment = 1530853200;
+let getDate = functions.getDate(date_moment);
 
 describe('Unit tests using Mocha', () => {
 
-  it('isNumber', () => {
-    chai.expect(_function.isNumber(2)).to.equal('number')
-  })
+  it('getAllDatesTest', () => {    
+    chai.assert.lengthOf(getAllDates, 42, 'DateList`s value has a length of 3');
 
-  it('isVowel', () => {
-    chai.expect(_function.isVowel('a')).to.equal(true)
+  })
+  it('getDateTest', () => {  
+    chai.expect(getAllDates.length).to.equal(42);
+    chai.expect(getDate.length).to.equal(1); 
+    chai.expect(getDate[0].moment).to.equal(date_moment);
   })
 
 })
